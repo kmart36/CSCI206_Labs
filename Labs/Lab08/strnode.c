da92a959db095c8041d9f10e678ee12e40ec6bfa
Martinson Katy Aisling <kam071@linuxremote1.bucknell.edu>
2022-03-08 11:51:58 -0500

    Lab 08 prelab fixed

--- Labs/Lab08/strnode.c
+++ Labs/Lab08/strnode.c
@@ -13,7 +13,7 @@ struct strnode *strnode_create(char *s, int length) {
   struct strnode * one_str_node = (struct strnode *)malloc(sizeof(struct strnode));
   one_str_node->str = (char*)malloc(strlen(s) + 1);
   strcpy(one_str_node->str, s);
-  one_str_node->length = strlen(s);
+  one_str_node->length = strlen(one_str_node->str) + 1;
   one_str_node->next = NULL;
   return one_str_node;
 } 
